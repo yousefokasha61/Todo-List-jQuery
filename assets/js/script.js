5588d71101517c70a2c91b7d0931215d23d1aa1b
yousef okasha <yousefokasha61@gmail.com>
2020-09-19 03:27:49 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// check off specific todos by clicking
+$('ul').on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+// click on X to delete todo
+$('ul').on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    })
+    event.stopPropagation();
+});
+
+$('input[type="text"]').keypress(function(event){
+    if(event.which === 13){
+        //storing the todo input in variable
+        var todo = $(this).val();
+        // clear the input from the todo
+        $(this).val("");
+        //create a new li and add to ul
+        $('ul').append("<li><span><i class='fas fa-trash-alt'></i></span> " + String(todo) + "</li>"); 
+    }
+});
+
+$('.fa-plus').click(function(){
+    $('input[type="text"]').fadeToggle();
+});
